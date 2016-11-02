f6b3dba0bb7018330b348f4086fcccacbba96a77
maltsev <remmdp@gmail.com>
2016-11-02 14:44:41 +0200

    commit

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.notes.adapters.NotesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.notes_recycler_view);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        NotesAdapter notesAdapter = new NotesAdapter();
+        List<String> dataSource = new ArrayList<>();
+        for (int i = 0; i<100; i++){
+            dataSource.add("title: " + i);
+        }
+
+        recyclerView.setAdapter(notesAdapter);
+        notesAdapter.setDataSource(dataSource);
     }
 }
